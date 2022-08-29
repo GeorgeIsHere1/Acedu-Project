f517b5b9baefc65d6616b561c4a4333063206388
George David <dgeorge16000@gmail.com>
2022-08-29 06:47:24 -0700

    add Acedu project frontend

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(".js--about-us-section").waypoint(function(direction) {
+		$(".js--about-box").addClass('animate__animated animate__fadeInUp animation');
+	}, {
+		offset:'50%'
+	}); 
+
+});
+    
+  
